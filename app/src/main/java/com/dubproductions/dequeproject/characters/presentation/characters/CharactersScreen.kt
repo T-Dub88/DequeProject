e5406e4931ede933bea9c3c9426a0e6fd7d1c9a4
Tyler Williams <tylerlw1988@gmail.com>
2024-06-26 17:40:46 -0400

    Edit image placement and size

--- app/src/main/java/com/dubproductions/dequeproject/characters/presentation/characters/CharactersScreen.kt
+++ app/src/main/java/com/dubproductions/dequeproject/characters/presentation/characters/CharactersScreen.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Card
@@ -18,11 +20,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
 import coil.compose.SubcomposeAsyncImage
 import com.dubproductions.dequeproject.characters.domain.model.CharacterImage
 import com.dubproductions.dequeproject.characters.domain.model.CharacterSummary
@@ -84,17 +85,24 @@ fun CharacterListDisplay(characterList: List<CharacterSummary>) {
                         .fillParentMaxWidth()
                         .padding(8.dp)
                 ) {
+                    SubcomposeAsyncImage(
+                        modifier = Modifier
+                            .clip(RoundedCornerShape(20.dp))
+                            .align(Alignment.CenterHorizontally)
+                            .width(200.dp),
+                        model = it.image?.thumbUrl,
+                        contentDescription = "Characters Image",
+                        loading = {
+                            CircularProgressIndicator(
+                                modifier = Modifier
+                                    .size(30.dp)
+                            )
+                        }
+                    )
                     Text(text = "Name: ${it.name}")
                     Text(text = "Gender: ${getGender(it.gender ?: 0)}")
                     Text(text = "First Game: ${it.firstGame?.name}")
                     Text(text = "Summary: ${it.deck}")
-                    SubcomposeAsyncImage(
-                        model = it.image?.thumbUrl,
-                        contentDescription = "Characters Image",
-                        loading = {
-                            CircularProgressIndicator()
-                        }
-                    )
                 }
             }
         }
